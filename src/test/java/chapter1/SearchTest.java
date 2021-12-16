150b3c5c122b7262c97f47a0cacc92f16d166ca9
artem <artemii.lulevich@gmail.com>
2021-12-16 17:00:34 +0200

    End locators

--- src/test/java/chapter1/SearchTest.java
+++ src/test/java/chapter1/SearchTest.java
@@ -9,9 +9,11 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class SearchTest {
 
-    WebDriver driver;
+    private WebDriver driver;
 
     @BeforeMethod
     public void setup() {
@@ -57,5 +59,67 @@ public class SearchTest {
         Assert.assertEquals(driver.getTitle(), "Search results for: 'Electronics'");
     }
 
+    @Test
+    public void byLinkText() {
+        WebElement myAccountLink = driver.findElement(By.linkText("MY ACCOUNT"));
+
+        myAccountLink.click();
+
+        Assert.assertEquals(driver.getTitle(), "Customer Login");
+    }
+
+    @Test
+    public void byPartialLinkText(){
+        WebElement orderAndReturns = driver.findElement(By.partialLinkText("PRIVACY"));
+
+        orderAndReturns.click();
+
+        Assert.assertEquals(driver.getTitle(), "Privacy Policy");
+    }
+
+    @Test
+    public void byTagNameLocator() {
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+
+        System.out.println("Found links: " + links.size());
+
+        links.stream()
+                .filter(elem -> elem.getText().length() > 0)
+                .forEach(elem -> System.out.println(elem.getText()));
+    }
+
+    @Test
+    public void byxPathLocator() {
+        WebElement searchBox = driver.findElement(By.xpath("//*[@id='search']"));
+
+//        One disadvantage of using XPath is that it is costly in terms of time. For every
+//        element to be identified, WebDriver actually scans through the entire page,
+//        which is very time-consuming, and too much usage of XPath in your test script
+//        will actually make it too slow to execute.
+
+        searchBox.sendKeys("Bags");
+        searchBox.submit();
+
+        Assert.assertEquals(driver.getTitle(), "Search results for: 'Bags'");
+    }
+
+    @Test
+    public void byCssSelector() {
+        WebElement searchBox = driver.findElement(By.cssSelector("#search"));
+
+
+        searchBox.sendKeys("Bags");
+        searchBox.submit();
+
+        Assert.assertEquals(driver.getTitle(), "Search results for: 'Bags'");
+    }
+
+    @Test
+    public void byCssSelectorComplex() {
+        WebElement aboutUs = driver.findElement(By.cssSelector("a[href*='/about-magento-demo-store/'"));
+
+        aboutUs.click();
+        Assert.assertEquals(driver.getTitle(), "About Us");
+    }
 
 }
