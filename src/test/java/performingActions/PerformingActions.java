338038153f9b6b52ec72c606c2c8fdded7a4bd79
artem <artemii.lulevich@gmail.com>
2021-12-24 19:06:26 +0200

    PerformingActions add.

---
+++ src/test/java/performingActions/PerformingActions.java
@@ -0,0 +1,56 @@
+package performingActions;
+
+import base.BaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PerformingActions extends BaseTest {
+
+    @Test
+    public void sendKeysExample() {
+
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("Phones");
+
+        WebElement searchButton = driver.findElement(By.className("search-button"));
+        searchButton.click();
+
+        Assert.assertEquals(driver.getTitle(), "Search results for: 'Phones'");
+    }
+
+    @Test
+    public void sendKeysCompositionExample() {
+
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys(Keys.chord(Keys.SHIFT,"Phones"));
+        searchBox.submit();
+
+        Assert.assertEquals(driver.getTitle(), "Search results for: 'PHONES'");
+
+    }
+
+    @Test
+    public void clearExample() {
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys(Keys.chord(Keys.SHIFT,"Phones"));
+        searchBox.clear();
+    }
+
+    @Test
+    public void submitExample() {
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys(Keys.chord(Keys.SHIFT,"Phones"));
+        searchBox.submit();
+    }
+
+    @Test
+    public void submitErrorExample() {
+        WebElement aboutUs = driver.findElement(By.linkText("ABOUT US"));
+        aboutUs.submit();
+
+    }
+
+}
