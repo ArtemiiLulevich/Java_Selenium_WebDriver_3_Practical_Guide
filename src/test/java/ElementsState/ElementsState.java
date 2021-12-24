7d9afe65c02b70eebc1216f104e77a3a35d99b54
artem <artemii.lulevich@gmail.com>
2021-12-24 19:23:02 +0200

    ElementsState add.

---
+++ src/test/java/ElementsState/ElementsState.java
@@ -0,0 +1,28 @@
+package ElementsState;
+
+import base.BaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class ElementsState extends BaseTest {
+
+    @Test
+    public void isDisplayedExample() {
+        WebElement searchBox = driver.findElement(By.name("q"));
+        System.out.println("Search box is displayed: " + searchBox.isDisplayed());
+    }
+
+    @Test
+    public void isEnabledExample() {
+        WebElement searchBox = driver.findElement(By.name("q"));
+        System.out.println("Search box is enabled: " + searchBox.isEnabled());
+    }
+
+    @Test
+    public void isSelectedExample() {
+        WebElement searchBox = driver.findElement(By.name("q"));
+        System.out.println("Search box is selected: " + searchBox.isSelected());
+    }
+
+}
